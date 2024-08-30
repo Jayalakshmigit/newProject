22fc4964c9db1cca892d9fbfe68471a74d6a22ff
Your Name <you@example.com>
2024-08-30 06:17:40 +0000

    weather

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require(`express`);
+const express = require("express");
 const app = express();
-const request = require(`request`);
+const request = require("request");
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   let city = req.query.city;
-  var request = require(`request`);
   request(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid={92ab6302688bb99a959762953a34514f}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={92ab6302688bb99a957962953a34514f}`,
     function (error, response, body) {
-       let data = Json.parse(body);
+      let data = JSON.parse(body);
       if (response.statusCode === 200) {
-        res.send(`The Weather description in the city "${city}" is ${data.list[0].weather[0].description}`);
+        res.send(
+          `Sky Conditions of "${city}" is ${data.list[0].weather[0].description}`
+        );
       }
-      );
+    }
+  );
 });
 
 app.listen(3000, () => console.log(`Server started on port 3000`));
